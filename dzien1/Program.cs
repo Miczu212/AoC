90c1dca6cef20a734360f4648576f3b8819a9289
Miczu212 <119628486+Miczu212@users.noreply.github.com>
2022-12-01 21:23:40 +0100

    Add files via upload

---
+++ dzien1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+
+namespace adventocode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int j = 0, suma = 0;
+            string[] line = System.IO.File.ReadAllLines(@"C:\Users\pc\Desktop\Studia\inne\adventocode\a.txt");
+            int[] sumaelfow = new int[line.Length];
+            for (int i = 0; i < line.Length; i++)
+            {
+
+
+                if (int.TryParse(line[i], out int value))
+
+                {
+                    suma += int.Parse(line[i]);
+                }
+                else
+                {
+                    sumaelfow[j] = suma;
+                    j++;
+                    suma = 0;
+                }
+
+            }
+            var sum = sumaelfow.ToList();
+            int g = j;
+            for (int i = 0; i <= 2; i++)
+            {
+                Console.WriteLine(sum.Max());
+                for(j=0;j<g;j++)
+                {
+                    if (sum[j] ==sum.Max())
+                    {
+                        sum.Remove(sum[j]);
+                        break;
+
+                    }
+                }
+            }
+
+        }
+    }
+}
