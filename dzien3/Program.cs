4afd16c2f7aedf5527bc71134857d365f96fc81d
Miczu212 <119628486+Miczu212@users.noreply.github.com>
2022-12-03 13:02:39 +0100

    Add files via upload

---
+++ dzien3/Program.cs
@@ -0,0 +1,74 @@
+﻿//using System;
+//using System.Linq;
+//using System.Text;
+
+//namespace dzien3
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            string[] plik = System.IO.File.ReadAllLines(@"C:\Users\pc\Desktop\Studia\inne\adventocode\a.txt");
+//            string[] linie = new string[plik.Length];
+//            int i = 0,punkty=0;
+//            bool polowa=false;
+//            char buff=' ';
+//            foreach (var i1 in plik)
+//                {
+//                linie[i] = i1;
+//                i++;
+//                }
+//            i = 0;
+
+//            for (int o = 0; o < linie.Length; o++)
+//            {
+//                char[] temp = new char[linie[o].Length/2];
+//                char[] temp2 = new char[linie[o].Length/2];
+//                foreach (var i1 in linie[o])
+//                {
+//                    if (polowa == false)
+//                    {
+//                        temp[i] = i1;
+//                        i++;
+//                    }                       
+//                    else
+//                    {
+//                        temp2[i] = i1;
+//                        i++;
+//                    }
+//                    if (i == linie[o].Length / 2)
+//                    {
+//                        polowa = true;
+//                        i = 0;
+//                    }
+//                }
+//                polowa = false;
+//                foreach (char a in temp)
+//                {
+//                    foreach (char b in temp2)
+//                    {
+//                        if (b.Equals(a))
+//                        {
+//                            buff = b;
+                            
+                            
+//                        }
+//                    }
+//                }
+                
+//                if ((int)buff >= 65 && (int)buff <= 90)
+//                {
+//                    punkty += (int)buff - 38;
+//                }
+//                else if ((int)buff >= 97 && (int)buff <= 122)
+//                {
+//                    punkty += buff - 96;
+//                }
+
+
+//            }
+//            Console.WriteLine(punkty);
+
+//        }
+//    }
+//}
---
+++ dzien3/Program1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace dzien3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] plik = System.IO.File.ReadAllLines(@"C:\Users\pc\Desktop\Studia\inne\adventocode\a.txt");
+            string[] linie = new string[plik.Length];
+            int i = 0, punkty = 0;
+            char buff;
+            bool isdone=false;
+            foreach (var i1 in plik)
+            {
+                linie[i] = i1;
+                i++;
+            }
+
+            for (int o = 0; o < linie.Length; o = o + 3)
+            {
+                foreach (char c in linie[o])
+                {
+                    foreach (char b in linie[o + 1])
+                    {
+                        foreach (char a in linie[o + 2])
+                        {
+                            if (a.Equals(b) && a.Equals(c))
+                            {
+                                buff = a;
+                                if (isdone == false)
+                                {
+                                    if ((int)buff >= 65 && (int)buff <= 90)
+                                    {
+                                        punkty += (int)buff - 38;
+                                    }
+                                    else if ((int)buff >= 97 && (int)buff <= 122)
+                                    {
+                                        punkty += (int)buff - 96;
+                                    }
+                                    isdone = true;   
+                                }
+                                
+                            }
+                        }
+                    }
+                }
+                isdone = false;
+            }
+            Console.WriteLine(punkty);
+        }
+    }
+}
